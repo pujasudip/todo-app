7ecb7cb1399b5ca3e72dd89c02127f9a5b5e8ea9
Sudip Baral <pujasudip@gmail.com>
2018-08-09 15:40:20 -0700

    adding item functionality working

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,50 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import React, {Component} from 'react';
 import TodoList from './todo_list';
+import AddItem from './add_item';
+import listData from '../data/todo';
 
-const App = () => (
-    <div className="container">
-        <h1 className="center">To Do List</h1>
-        <TodoList/>
-    </div>
-);
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            items: []
+        }
+    }
+
+    componentDidMount(){
+        this.getListData();
+    }
+
+    getListData(){
+        // This is where we would call the server for our data
+        this.setState({
+            items: listData
+        });
+    }
+
+    addItem(item){
+        item._id = new Date().getTime();
+
+        this.setState({
+            items: [item, ...this.state.items]
+        });
+
+        console.log(item);
+    }
+
+    render(){
+        console.log('state:', this.state);
+
+        return (
+            <div className="container">
+                <h1 className="center">To Do List</h1>
+                <AddItem add={this.addItem.bind(this)}/>
+                <TodoList list={this.state.items}/>
+            </div>
+        );
+    }
+}
 
 export default App;
