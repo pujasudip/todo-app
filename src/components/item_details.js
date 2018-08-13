d44d9930eb3c532fb245222d0f0fd708ed713464
Sudip Baral <pujasudip@gmail.com>
2018-08-13 16:17:28 -0700

    todo-item detail shows up

--- src/components/item_details.js
+++ src/components/item_details.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import config from '../config';
+import axios from "axios";
 
 class ItemDetails extends Component {
+    state = {
+        itemDetails: {}
+    }
 
-    componentDidMount(){
-        const id =  this.props.match.params.item_id;
+    async componentDidMount(){
+        const {BASE_URL, API_KEY} = config.api;
+        const {item_id} =  this.props.match.params;
+
+        const resp = await axios.get(`${BASE_URL}/todos/${item_id}${API_KEY}`);
+
+        this.setState({
+            itemDetails: resp.data.todo
+        });
     }
 
     render(){
+        const { itemDetails } = this.state;
+        console.log(itemDetails);
+
         return (
             <div>
                 <h1 className='center'>Item Details</h1>
@@ -17,6 +31,7 @@ class ItemDetails extends Component {
                         <Link to="/" className='btn purple darken-2'>Back to List</Link>
                     </div>
                 </div>
+                <h4><em>Title: </em>{itemDetails.title}</h4>
             </div>
         );
     }
